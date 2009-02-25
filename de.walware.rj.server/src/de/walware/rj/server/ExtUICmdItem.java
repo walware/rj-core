775e73326b6760e4ab599f9eb18c308a24f289e1
Stephan Wahlbrink <sw@wahlbrink.eu>
2009-02-25 20:19:17 +0100

    [*] Updated copyright for 2009
    Signed-off-by: Stephan Wahlbrink <sw@wahlbrink.eu>

--- de.walware.rj.server/src/de/walware/rj/server/ExtUICmdItem.java
+++ de.walware.rj.server/src/de/walware/rj/server/ExtUICmdItem.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2008 Stephan Wahlbrink and others.
+ * Copyright (c) 2008-2009 Stephan Wahlbrink and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the GNU Lesser General Public License
  * v2.1 or newer, which accompanies this distribution, and is available at
--- de.walware.rj.server/srcRosuda/org/rosuda/rj/JRClassLoader.java
+++ de.walware.rj.server/srcRosuda/org/rosuda/rj/JRClassLoader.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2005-2008 RoSuDa (www.rosuda.org) and others.
+ * Copyright (c) 2005-2009 RoSuDa (www.rosuda.org) and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the GNU Lesser General Public License
  * v2.1 or newer, which accompanies this distribution, and is available at
