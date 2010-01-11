d3ed0d8969396e3573946c01a9fdd457d1ed2b24
Stephan Wahlbrink <sw@wahlbrink.eu>
2010-01-11 22:53:11 +0100

    [*] Updated copyright for 2010
    Signed-off-by: Stephan Wahlbrink <sw@wahlbrink.eu>

--- de.walware.rj.server/srcEExtensions/de/walware/rj/server/srvext/EServerUtil.java
+++ de.walware.rj.server/srcEExtensions/de/walware/rj/server/srvext/EServerUtil.java
@@ -1,6 +1,6 @@
 /*******************************************************************************
- * Copyright (c) 2009 Stephan Wahlbrink and others.
- * All rights reserved. This program and the accompanying materials
+ * Copyright (c) 2009-2010 Stephan Wahlbrink (www.walware.de/goto/opensource)
+ * and others. All rights reserved. This program and the accompanying materials
  * are made available under the terms of the GNU Lesser General Public License
  * v2.1 or newer, which accompanies this distribution, and is available at
  * http://www.gnu.org/licenses/lgpl.html
