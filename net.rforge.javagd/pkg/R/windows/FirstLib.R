86434ce2acda556db03045a5b4185398985e7eda
JavaGD Project <rforge.net/JavaGD>
2011-06-03 22:31:57 +0200

    [JavaGD] Added JavaGD-0.5.3 (svn88/3166)
    Signed-off-by: Stephan Wahlbrink <sw@wahlbrink.eu>

---
+++ net.rforge.javagd/pkg/R/windows/FirstLib.R
@@ -0,0 +1,49 @@
+.First.lib <-
+function(libname, pkgname) {
+    javahome <- Sys.getenv("JAVA_HOME")
+    .putenv <- if (exists("Sys.setenv")) Sys.setenv else Sys.putenv
+    if(!nchar(javahome)) {
+	# let's try to fetch the paths from registry via WinRegistry.dll
+	javahome <- NULL
+	library.dynam("WinRegistry", pkgname, libname)
+	key<-"Software\\JavaSoft\\Java Runtime Environment"
+	jrever <- .Call("RegGetStrValue",c(key,"CurrentVersion"))
+	if (is.null(jrever)) { # try JDK if JRE fails
+	    key<-"Software\\JavaSoft\\Java Development Kit"
+	    jrever <- .Call("RegGetStrValue",c(key,"CurrentVersion"))
+	}
+	if (!is.null(jrever)) {
+	    dispver <- jrever
+	    key<-paste(key,jrever,sep="\\")
+	    micro <- .Call("RegGetStrValue", c(key,"MicroVersion"))
+	    if (!is.null(micro)) dispver <- paste(dispver,micro,sep=".")
+#	    cat("using Java Runtime version",dispver,"\n")
+	    javahome <- .Call("RegGetStrValue",c(key,"JavaHome"))
+	    if (!is.null(javahome)) { # ok, let's try to get the real lib path
+		p <- .Call("RegGetStrValue",c(key,"RuntimeLib"))
+		if (!is.null(p)) {
+		    # the following assumes that the entry is of the form
+		    # ...\jvm.dll - this should be ok since if it's not,
+		    # then we won't find the DLL either.
+		    # Note that we just add it to the PATH so if this fails
+		    # we still fall back to the JavaHome entry.
+		    .putenv(PATH=paste(Sys.getenv("PATH"),
+					  substr(p,1,nchar(p)-8),sep=";"))
+		}
+	    }
+	}
+	if (is.null(javahome))
+	    stop("JAVA_HOME is not set")
+    }
+    if(!nchar(javahome)) stop("JAVA_HOME is not set")
+#    else cat("using JAVA_HOME =", javahome, "\n")
+    .putenv(PATH=paste(Sys.getenv("PATH"),
+               file.path(javahome, "bin", "client"),
+               file.path(javahome, "bin", "server"),
+               file.path(javahome, "bin"),
+	       sep=";"))
+    .javaGD.jar.file<-paste(libname,pkgname,"java","javaGD.jar",sep=.Platform$file.sep)
+    library.dynam("JavaGD", pkgname, libname)
+    # set internal classpath in case we really need to initialize the JVM
+    .C("setJavaGDClassPath",paste(.javaGD.jar.file,Sys.getenv("CLASSPATH"),sep=";"))
+}
