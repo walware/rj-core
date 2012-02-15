e366d9a68433dad48c444082fdc0b62dcec89246
Stephan Wahlbrink <sw@wahlbrink.eu>
2012-02-15 18:53:26 +0100

    [*] Updated copyright for 2012
    Signed-off-by: Stephan Wahlbrink <sw@wahlbrink.eu>

--- de.walware.rj.server/src/de/walware/rj/server/dbg/DbgFilterState.java
+++ de.walware.rj.server/src/de/walware/rj/server/dbg/DbgFilterState.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2011 Stephan Wahlbrink (www.walware.de/goto/opensource)
+ * Copyright (c) 2011-2012 Stephan Wahlbrink (www.walware.de/goto/opensource)
  * and others. All rights reserved. This program and the accompanying materials
  * are made available under the terms of the GNU Lesser General Public License
  * v2.1 or newer, which accompanies this distribution, and is available at
--- de.walware.rj.server/srcJRI/RJavaClassLoader.java
+++ de.walware.rj.server/srcJRI/RJavaClassLoader.java
@@ -1,6 +1,6 @@
 /*******************************************************************************
- * Copyright (c) 2005-2011 RoSuDa (www.rosuda.org) and others.
- * and others. All rights reserved. This program and the accompanying materials
+ * Copyright (c) 2005-2012 RoSuDa (www.rosuda.org) and others.
+ * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the GNU Lesser General Public License
  * v2.1 or newer, which accompanies this distribution, and is available at
  * http://www.gnu.org/licenses/lgpl.html
