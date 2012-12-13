dcafe0d2e54920553fb061cb24b2be8b7b7df289
Stephan Wahlbrink <sw@wahlbrink.eu>
2012-12-13 11:43:33 +0100

    [RJ-RPkg] Added support to open R elements (e.g. in data viewer) using function 'openInEditor'
    Signed-off-by: Stephan Wahlbrink <sw@wahlbrink.eu>

--- de.walware.rj.rpkg/pkg/R/statet_workbench.R
+++ de.walware.rj.rpkg/pkg/R/statet_workbench.R
@@ -4,12 +4,49 @@
 #' @param filename name of file to open
 #' 
 #' @export
-openInEditor <- function(filename) {
-	if (missing(filename) || !is.character(filename) || length(filename) != 1) {
-		stop("Illegal argument: filename")
+openInEditor <- function(name, fileName, elementName, filename) {
+	if (!missing(filename)) {
+		fileName <- filename
 	}
-	.rj_ui.execCommand("common/showFile", list(
-					filename= filename ), wait= TRUE)
+	if (missing(name) && missing(fileName) && missing(elementName)) {
+		stop("Missing argument: name")
+	}
+	if (missing(fileName) && missing(elementName)) {
+		# auto
+		fileName <- NULL
+		elementName <- NULL
+		if (is.character(name) && length(name) == 1) {
+			fileName <- name
+		}
+		else {
+			elementName <- substitute(name)
+		}
+	}
+	else if (!missing(elementName)) {
+		fileName <- NULL
+		elementName # test, if it refers to an existing element
+		elementName <- substitute(elementName)
+	}
+	else if (!missing(fileName)) {
+		elementName <- NULL
+	}
+	
+	if (!is.null(fileName)) {
+		if (!is.character(fileName) || length(fileName) != 1) {
+			stop("Illegal argument: fileName")
+		}
+		.rj_ui.execCommand("common/showFile", list(
+						filename= fileName ), wait= TRUE)
+	}
+	else {
+		elementName <- deparse(elementName, backtick= TRUE, control= c(),
+				width.cutoff= 500, nlines= 2L)
+		if (length(elementName) != 1) {
+			stop("Illegal argument: elementName")
+		}
+		.rj_ui.execCommand("showElement", list(
+						elementName= elementName ), wait= TRUE)
+	}
 	return (invisible())
 }
 
