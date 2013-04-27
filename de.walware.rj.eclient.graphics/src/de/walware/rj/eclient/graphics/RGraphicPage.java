0c776de7c44414304ff6e7500686d6e44958d9ac
Stephan Wahlbrink <sw@wahlbrink.eu>
2013-04-27 17:03:43 +0200

    [R-Graphics] Fixed locator info in status line when page was opened in background
    Signed-off-by: Stephan Wahlbrink <sw@wahlbrink.eu>

--- de.walware.rj.eclient.graphics/src/de/walware/rj/eclient/graphics/RGraphicPage.java
+++ de.walware.rj.eclient.graphics/src/de/walware/rj/eclient/graphics/RGraphicPage.java
@@ -33,6 +33,7 @@ import org.eclipse.ui.services.IServiceLocator;
 
 import de.walware.ecommons.IStatusChangeListener;
 import de.walware.ecommons.ui.SharedUIResources;
+import de.walware.ecommons.ui.util.UIAccess;
 
 import de.walware.rj.eclient.graphics.RGraphicCompositeActionSet.LocationListener;
 
@@ -49,7 +50,6 @@ public class RGraphicPage extends Page implements IStatusChangeListener {
 	
 	private RGraphicCompositeActionSet fActions;
 	
-	private boolean fActivated;
 	private StatusLineContributionItem fLocationStatusItem;
 	private String fLocationStatusText;
 	private LocationListener fMouseLocationListener;
@@ -70,13 +70,7 @@ public class RGraphicPage extends Page implements IStatusChangeListener {
 	
 	@Override
 	public void createControl(final Composite parent) {
-		fControl = new RGraphicComposite(parent, fGraphic) {
-			@Override
-			public void setVisible(final boolean visible) {
-				super.setVisible(visible);
-				updateState();
-			}
-		};
+		fControl = new RGraphicComposite(parent, fGraphic);
 		
 		initActions(getSite(), getSite().getActionBars());
 		
@@ -110,7 +104,7 @@ public class RGraphicPage extends Page implements IStatusChangeListener {
 		if (fLocationStatusItem != null) {
 			fMouseLocationListener = new LocationListener() {
 				
-				final DecimalFormat format = new DecimalFormat("0.0####", new DecimalFormatSymbols(Locale.US));
+				final DecimalFormat format = new DecimalFormat("0.0####", new DecimalFormatSymbols(Locale.US)); //$NON-NLS-1$
 				
 				@Override
 				public void loading() {
@@ -121,44 +115,30 @@ public class RGraphicPage extends Page implements IStatusChangeListener {
 				
 				@Override
 				public void located(final double x, final double y) {
-					if (fLocationStatusItem != null) {
+					if (fLocationStatusItem != null && UIAccess.isOkToUse(fControl)) {
 						if (Double.isNaN(x) || Double.isInfinite(x)
 								|| Double.isNaN(y) || Double.isInfinite(y) ) {
-							fLocationStatusText = "NA";
+							fLocationStatusText = "NA"; //$NON-NLS-1$
 						}
 						else {
-							fLocationStatusText = "(" + format.format(x) + ", " + format.format(y) + ")"; //$NON-NLS-1$
+							final StringBuilder sb = new StringBuilder(32);
+							sb.append('(');
+							sb.append(format.format(x));
+							sb.append(", "); //$NON-NLS-1$
+							sb.append(format.format(y));
+							sb.append(')');
+							fLocationStatusText = sb.toString();
 						}
-						if (fActivated) {
-							fLocationStatusItem.setText(fLocationStatusText);
-						}
+						fLocationStatusItem.setText(fLocationStatusText);
 					}
 				}
 			};
 			fActions.addMouseClickLocationListener(fMouseLocationListener);
 		}
-		
-		updateState();
 	}
 	
 	protected RGraphicCompositeActionSet createActionSet() {
 		return new RGraphicCompositeActionSet(fControl);
-	}
-	
-	private void updateState() {
-		if (fControl != null && !fControl.isDisposed() && fControl.isVisible()) {
-			if (!fActivated) {
-				fActivated = true;
-				if (fLocationStatusItem != null) {
-					fLocationStatusItem.setText((fLocationStatusText != null) ? fLocationStatusText : ""); //$NON-NLS-1$
-				}
-			}
-		}
-		else {
-			if (fActivated) {
-				fActivated = false;
-			}
-		}
 	}
 	
 	@Override
