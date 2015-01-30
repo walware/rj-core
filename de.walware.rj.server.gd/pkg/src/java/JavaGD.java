e4420bbd67cfc37923faf3f2650becea5909fdf4
Stephan Wahlbrink <sw@wahlbrink.eu>
2015-01-30 12:23:51 +0100

    Updated copyright for 2015
    Signed-off-by: Stephan Wahlbrink <sw@wahlbrink.eu>

--- de.walware.rj.server.gd/pkg/src/java/JavaGD.java
+++ de.walware.rj.server.gd/pkg/src/java/JavaGD.java
@@ -2,7 +2,7 @@
 //  JavaGD - Java Graphics Device for R
 //  JavaGD.java - default GDInterface implementation for use in JavaGD
 // 
-//  Copyright (C) 2004-2014  Simon Urbanek
+//  Copyright (c) 2004-2015  Simon Urbanek
 // 
 //  This library is free software; you can redistribute it and/or
 //  modify it under the terms of the GNU Lesser General Public
