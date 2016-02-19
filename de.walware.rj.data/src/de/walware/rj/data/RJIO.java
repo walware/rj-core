6a5f6210aa0df2f7a4044f94c527a1f685757e37
Stephan Wahlbrink <sw@wahlbrink.eu>
2016-02-19 15:31:49 +0100

    Added EPL option
    Signed-off-by: Stephan Wahlbrink <sw@wahlbrink.eu>

--- de.walware.rj.data/src/de/walware/rj/data/RJIO.java
+++ de.walware.rj.data/src/de/walware/rj/data/RJIO.java
@@ -1,9 +1,13 @@
 /*=============================================================================#
  # Copyright (c) 2010-2016 Stephan Wahlbrink (WalWare.de) and others.
  # All rights reserved. This program and the accompanying materials
- # are made available under the terms of the GNU Lesser General Public License
- # v2.1 or newer, which accompanies this distribution, and is available at
- # http://www.gnu.org/licenses/lgpl.html
+ # are made available under the terms of either (per the licensee's choosing)
+ #   - the Eclipse Public License v1.0
+ #     which accompanies this distribution, and is available at
+ #     http://www.eclipse.org/legal/epl-v10.html, or
+ #   - the GNU Lesser General Public License v2.1 or newer
+ #     which accompanies this distribution, and is available at
+ #     http://www.gnu.org/licenses/lgpl.html
  # 
  # Contributors:
  #     Stephan Wahlbrink - initial API and implementation
